3bdaa5aabf1b3d75ff3387280effd5f539ca34de
Haris Begić <harisbegic01@hotmail.com>
2021-03-11 13:17:13 +0100

    Fourth task Day04

---
+++ ex04/src/index.js
@@ -0,0 +1,12 @@
+function main(){
+    var myDecimal=8.2;
+    myDecimalE= 0.00254;
+
+    
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+console.log(main());
+module.exports = main;
