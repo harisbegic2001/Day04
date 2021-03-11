e74de85d5a6ac8ab6ad91f0158f75fdd8af04520
Haris Begić <harisbegic01@hotmail.com>
2021-03-11 13:51:58 +0100

    Tenth task Day04

---
+++ ex10/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+
+    var myFirstName='Haris';
+    var myLastName='Begić';
+    var myHomeTown='Sarajevo';
+    return{
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+console.log(main());
+module.exports = main;
